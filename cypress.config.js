0cb97c8484b2323fb3c76a7392092c6a294bd709
Cxva97 <cxva97@hotmail.com>
2026-01-08 16:52:53 -0500

    selectores

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 692,
+  viewportWidth: 1111,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/1-getting-started/laboratorio.cy.js
+++ cypress/e2e/1-getting-started/laboratorio.cy.js
@@ -13,6 +13,21 @@ describe('example to-do app', () => {
     cy.get('[data-at="empty-cart"]').click()
     cy.get('.text-black.text-center').should('have.text', 'Total: $0.00')
   })
+
+  it('find elements by text', () =>{
+    cy.contains('Registrarse').click()
+  })
+
+  it('find elements by atribut', () =>{
+    cy.get('[data-at="increment-quantity"]').click()
+    cy.get('[data-at="add-to-cart"]').click()
+  })
+
+  it.only('find elements by sons', () =>{
+    cy.get('[data-at="header"]').find('ul > li').eq(1).click()
+    // cy.get('[data-at="header"]').find('ul > li:nth-child(2)').click()  segunda manera de hacerlo
+    
+  })
 })
 
 //hola
