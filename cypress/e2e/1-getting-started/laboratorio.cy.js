29772591694ca6472d18ab199c3fa98b24ce92f6
Cxva97 <cxva97@hotmail.com>
2026-01-07 21:03:24 -0500

    practicando

--- cypress/e2e/1-getting-started/laboratorio.cy.js
+++ cypress/e2e/1-getting-started/laboratorio.cy.js
@@ -6,11 +6,12 @@ describe('example to-do app', () => {
 
   it('displays two todo items by default', () => {
     cy.get('.inline-block').click()
-    cy.contains('Añadir al carrito').click()
-    cy.get('[data-at="cart-opener-mobile"]').click()
-    cy.get('.py-5.overflow-auto').should('contains.text', 'Bandas Elásticas de Resistencia' )
-    cy.get('.text-black.text-center').should('have.text', 'Total: $350.00')
-    cy.get('[data-at="empty-cart"]').click()
-    cy.get('.text-black.text-center').should('have.text', 'Total: $0.00')
+    cy.get('a[href="/products/set-de-pesas-ajustables"]').closest('article')
+    .contains('button', 'Añadir al carrito')
+    .click()
+    cy.get('[data-at="cart-opener"]').click()
+
+    
+
   })
 })
