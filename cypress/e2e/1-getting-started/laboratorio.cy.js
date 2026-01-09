351e22b7c0801af225681fd27933955dedc71256
Cxva97 <cxva97@hotmail.com>
2026-01-08 19:26:00 -0500

    tipos de selectores

--- cypress/e2e/1-getting-started/laboratorio.cy.js
+++ cypress/e2e/1-getting-started/laboratorio.cy.js
@@ -1,7 +1,10 @@
 /// <reference types="cypress" />
 describe('example to-do app', () => {
   beforeEach(() => {
-    cy.visit('https://www.laboratoriodetesting.com/')
+    cy.setCookie('__AUTH-TOKEN-APP', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImQ3Y2E5NDJjLThlZDktNGIxNy05NTJjLWU2YzVhNGI1MmEwNyIsImlhdCI6MTc2NzkwOTkwNiwiZXhwIjoxNzcwNTAxOTA2fQ.357waHqowaL6Rn8cQab-KFnzgkQlVc5LDGSO-EDgQaA',
+      {secure:true}
+    )
+    cy.visit('https://www.laboratoriodetesting.com/form-practice')
   })
 
   it('displays two todo items by default', () => {
@@ -23,10 +26,50 @@ describe('example to-do app', () => {
     cy.get('[data-at="add-to-cart"]').click()
   })
 
-  it.only('find elements by sons', () =>{
+  it('find elements by sons', () =>{
     cy.get('[data-at="header"]').find('ul > li').eq(1).click()
-    // cy.get('[data-at="header"]').find('ul > li:nth-child(2)').click()  segunda manera de hacerlo
-    
+    // cy.get('[data-at="header"]').find('ul > li:nth-child(2)').click()  segunda manera de hacerlo 
+  })
+
+  it('check and uncheck', ()=>{
+    cy.get('[id="Pruebas manuales"]').check()
+    cy.get('[data-at="practice-interests-devops"]').check()
+    cy.get('[id="Pruebas manuales"]').should('be.checked')
+    cy.get('[id="Desarrollo de software"]').should('not.be.checked')
+  })
+
+  it('manejo de dropdown', ()=>{
+    cy.get('[data-at="practice-country"]').select('Colombia')
+    cy.get('[data-at="practice-country"]').should('be.visible')
+    .and('have.value', 'Colombia')
+  })
+
+  it('manejo de radio buttons', ()=>{
+    cy.get('#sch_Mañana').click()
+    cy.get('#sch_Mañana').should('be.checked')
+    cy.get('#sch_Tarde').should('not.be.checked')
+    cy.get('#sch_Noche').should('not.be.checked')
+  })
+
+  it('write in camps', ()=>{
+    cy.get('[data-at="practice-name"]').type('Cesar') //comando buscado por mi mismo
+    // {force:true} sirve para forzar la accion en caso de que el elemento este oculto o deshabilitado
+    cy.get('[name="password"]').type('1q2w3e{enter}')  //comando que me dio cypress
+  })
+
+  it.only('llenar formulario y enviar', ()=>{
+    cy.get('[data-at="practice-submit"]').should('be.disabled')
+    cy.get('[data-at="practice-name"]').type('Cesar')
+    cy.get('[name="password"]').type('1q2w3e')
+    cy.get('#sch_Mañana').click()
+    cy.get('[data-at="practice-country"]').select('Colombia')
+    cy.get('[id="Pruebas manuales"]').check()
+    cy.get('[data-at="practice-interests-devops"]').check()
+    cy.get('[data-at="practice-dateOfBirth"]').type('1997-04-10') //truco, escribir acorde al formato que indica en el DOM
+    cy.get('[data-at="practice-submit"]').should('be.enabled')
+    cy.get('[data-at="practice-submit"]').click()
+    cy.get('.swal2-confirm').click() //cypress 
+    // .swal2-confirm.swal2-styled.swal2-default-outline (propia busqueda)
   })
 })
 
